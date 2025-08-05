28f0952e65b7c44977ae53feb72ac5da813ea998
electr1fy0 <electr1fy0@icloud.com>
2025-08-06 00:11:42 +0530

    feat: add clean command to empty the vault

--- cmd/clean.go
+++ cmd/clean.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/x/term"
+	"github.com/electr1fy0/encraft/storage"
 	"github.com/spf13/cobra"
 )
 
@@ -8,7 +13,36 @@ var cleanCmd = &cobra.Command{
 	Use:   "clean",
 	Short: "delete all items from the vault",
 	Run: func(cmd *cobra.Command, args []string) {
+		exists, err := storage.VaultExists()
 
+		if err != nil {
+			fmt.Println("Error checking vault's presence: ", err)
+			os.Exit(1)
+		}
+
+		if !exists {
+			fmt.Println("No vault inside home directory. Initialize it first")
+			os.Exit(1)
+
+		}
+
+		fmt.Print("Enter master password: ")
+		passwordBytes, err := term.ReadPassword(0)
+		password := string(passwordBytes)
+
+		vault, err := storage.LoadVault(password)
+		if err != nil {
+			fmt.Println("Error loading the vault")
+			os.Exit(1)
+		}
+
+		for _, entry := range vault.Entries {
+			vault.DeleteEntry(entry.Name)
+		}
+
+		err = storage.SaveVault(vault, password)
+
+		fmt.Println("\nVault is now empty")
 	},
 }
 
